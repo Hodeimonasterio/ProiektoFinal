cabf4d0d646c6dbe3e1692bdf601204f814ee90a
Aritz <izarcelaya.artiz@uni.eus>
2022-05-16 20:31:03 +0200

    asp
    web asp

---
+++ Rest Proiektua/WebPokemon/WebPokemon/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using WebPokemon.Models;
+
+namespace WebPokemon.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<ActionResult> ListPokemon()
+        {
+            List<Pokemon> listaPokemon = new List<Pokemon>();
+
+            //String url = "https://localhost:44383/api/pokedex/pokemons";
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44383/");
+
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                // New code:
+                HttpResponseMessage response = await client.GetAsync("api/pokedex/pokemons");
+                if (response.IsSuccessStatusCode)
+                {
+                    var PokResponse = response.Content.ReadAsStringAsync().Result;
+                    listaPokemon = JsonConvert.DeserializeObject<List<Pokemon>>(PokResponse);
+
+                }
+            }
+
+            ViewBag.pokemons = listaPokemon;
+            return View();
+
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Rest Proiektua/WebPokemon/WebPokemon/Models/Pokemon.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebPokemon.Models
+{
+    public class Pokemon
+    {
+        public int id { get; set; }
+        public string num { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string img { get; set; }
+        [Required]
+        public string type1 { get; set; }
+        [Required]
+        public string type2 { get; set; }
+
+        [Required]
+        public string height { get; set; }
+        [Required]
+        public string weight { get; set; }
+
+
+        public Pokemon()
+        {
+        }
+    }
+}
